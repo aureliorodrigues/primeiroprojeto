89bacd319bd21e6d8f661c03c153729fd56b32d9
marcosil <marcosil@WORCESTER.sap.sp.gov.br>
2024-08-11 09:28:57 -0300

    alteracao no carro

--- primeiroprojeto/src/br/com/ford/Carro.java
+++ primeiroprojeto/src/br/com/ford/Carro.java
@@ -3,6 +3,7 @@ package br.com.ford;
 public class Carro {
 	
 	
+	
 	private String marca;
 	private String modelo;
 	private int ano;
@@ -25,6 +26,14 @@ public class Carro {
 	}
 	public void setAno(int ano) {
 		this.ano = ano;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "Carro [marca=" + marca + ", modelo=" + modelo + ", ano=" + ano + ", getMarca()=" + getMarca()
+				+ ", getModelo()=" + getModelo() + ", getAno()=" + getAno() + ", getClass()=" + getClass()
+				+ ", hashCode()=" + hashCode() + ", toString()=" + super.toString() + "]";
 	}
 	
 	
